9fe9d0006085dfe6effd0e4d4db795f39270aafe
Brian Magaha <btmy87@gmail.com>
2025-06-17 20:31:17 -0400

    Adding working implementation of hashFile and test cases

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # matlabHash
 Generate file hash values within MATLAB
+
+`out = hashFile(filename)` Generates a SHA256 hash of the given file.
---
+++ hashFile.m
@@ -0,0 +1,27 @@
+function out= hashFile(filename, opts)
+%HASHFILE generates hash of file
+% Generates hash of the given file, default is SHA256.  Any hash algorithm 
+% in the .NET System.Security.Cryptography namespace is valid.  Common
+% options include SHA1, SHA256, SHA384, SHA512, MD1
+%
+% out = hashFile(filename) generates the SHA256 hash of the given file
+%
+% out = hashFile(filename, hashAlgorithm="SHA1") generates the SHA1 hash of
+% the given file
+%
+% see test_hashFile for concrete usage examples
+
+arguments (Input)
+    filename (1, 1) string
+    opts.hashAlgorithm = "SHA256";
+end
+
+assert(exist(filename, "file") > 0, "file does not exist" + filename)
+
+fid = fopen(filename, "rb");
+data = fread(fid, "uint8=>uint8");
+hash = System.Security.Cryptography.(opts.hashAlgorithm).Create();
+outbytes = hash.ComputeHash(data);
+out = strjoin(string(dec2hex(uint8(outbytes))), "");
+
+end
