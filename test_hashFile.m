ac3f0a0a1860be8d1203f0b2ee9ceaa06feb9aea
Brian Magaha <btmy87@gmail.com>
2025-06-24 17:42:34 -0400

    Updating hash test values for LF line endings

--- test_hashFile.m
+++ test_hashFile.m
@@ -6,26 +6,26 @@ NET.addAssembly("System.Security.Cryptography");
 dotnetenv
 
 %% Test 1: Default, SHA256
-expectedValue = "436a6d536138f203ac333858cff92a568be62797752b3adb94bcaa0f6ffe7ef6";
+expectedValue = "6b0382b16279f26ff69014300541967a356a666eb0b91b422f6862f6b7dad17e";
 out = hashFile("LICENSE");
 assert(lower(out) == lower(expectedValue), "%s does not equal %s", lower(out), lower(expectedValue));
 
 %% Test 2: SHA1
-expectedValue = "e94b688646f4129635b82668543ef594c079e972";
+expectedValue = "6d842099530d126dea37db858a755e444f4de3f7";
 out = hashFile("LICENSE", hashAlgorithm="SHA1");
 assert(lower(out) == lower(expectedValue), "%s does not equal %s", lower(out), lower(expectedValue));
 
 %% Test 3: MD5
-expectedValue = "d512eeaf0d5285acc53d4569054cbb08";
+expectedValue = "d88e9e08385d2a17052dac348bde4bc1";
 out = hashFile("LICENSE", hashAlgorithm="MD5");
 assert(lower(out) == lower(expectedValue), "%s does not equal %s", lower(out), lower(expectedValue));
 
 %% Test 4: SHA384
-expectedValue = "b46c585bf6dd9e22504355ed6f4455322a41b3e01da8eb21f7295fc0404d3e41bdb89828bf2db724473c83679456dc3a";
+expectedValue = "f0ebf99d7a986f804027bc72c5421f33f1ea1b772d0ce34aa5301d809da2b17562e836945157d8d1af82bab56e991e51";
 out = hashFile("LICENSE", hashAlgorithm="SHA384");
 assert(lower(out) == lower(expectedValue), "%s does not equal %s", lower(out), lower(expectedValue));
 
 %% Test 5: SHA512
-expectedValue = "7d2c41db2fc891a5327798a1eb048093e25356ada8b7074308df69d85a51d2fb057b2998e38aba6277e983de486790f60d3b8b7cab70030ab2b779e19c7fcf46";
+expectedValue = "f23a3f3ec79340594ea40d731087e8586326a96c6cb9675c7a58687255885148286aa73cee087a5a3c976ea901fde16e229d00e98c65188cce3f0d75c8390a10";
 out = hashFile("LICENSE", hashAlgorithm="SHA512");
 assert(lower(out) == lower(expectedValue), "%s does not equal %s", lower(out), lower(expectedValue));
